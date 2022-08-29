b64a24ae852884df9d0974dadad5773150eb2c22
Rocio Chan Perochena <logistica3@ms.local>
2022-08-29 09:00:02 -0500

    Descomposicion de entero en millares, centenas, decenas y unidades en formato cadena

--- first_part.py
+++ first_part.py
@@ -8,8 +8,48 @@ class RomanNumberError(Exception):
     pass
 
 numero_romano = (
-    (1000, 'M'), (500, 'D'), (100, 'C'),(50, 'D'),(10, 'X'),(5, 'V'), (1, 'I'),
+    (1000, 'M'), (500, 'D'), (100, 'C'),(50, 'L'),(10, 'X'),(5, 'V'), (1, 'I'),
+)
+
+centenas = (
+    (100,'C'), (200,'CC'), (300,'CCC'),
+    (400,'CD'), (500,'D'),(600,'DC'), 
+    (700,'DCC'), (800,'DCCC'), (900,'CM')  
+)
+
+decenas = (
+    (10,'X'), (20,'XX'), (30,'XXX'), 
+    (40,'XL'), (50,'L'), (60,'LX'), 
+    (70,'LXX'), (80,'LXXX'), (90,'XC'), 
+)
+
+unidades = (
+    (1,'I'), (2,'II'), (3,'III'), 
+    (4,'IV'), (5,'V'), (6,'VI'), 
+    (7,'VII'), (8,'VIII'), (9,'IX')
 )
 
 def entero_a_romano(numero):
-    return "MCMXCIV"
+    '''
+    numero = str(numero)
+    longitud = len(numero)
+    
+    if longitud < 4:
+        numero = "{:0>4s}".format(numero)
+    '''
+    numero = "{:0>4d}".format(numero)
+    digitos = list(numero)
+
+    ix = 0
+    longitud = len(digitos)
+    for n in numero:
+        longitud -= 1
+        digitos[ix] = digitos[ix] + "0" * longitud
+        ix += 1
+
+    ''' otra forma seria 
+    for ix in range(longitud)'''
+   
+    return digitos
+
+entero_a_romano(336)
--- test_entero_a_romano.py
+++ test_entero_a_romano.py
@@ -1,5 +1,3 @@
-from first_part import entero_a_romano
-
 '''
 Casos de prueba
 a) 1994 -> MCMXCIV
@@ -9,12 +7,7 @@ d) 0 -> RomanNumberError ("El valor debe ser mayor a cero")
 e) -3 -> RomanNumberError ("El valor debe ser mayor a cero")
 f) 4.5 -> RomanNumberError ("El valor debe ser un entero")
 '''
+from first_part import entero_a_romano
 
-def test_3_es_igual_a_2_mas_uno():
-    assert 2 + 1 == 3
-
-def test_valor_1994():
-    assert entero_a_romano(1994) == "MCMXCIV"
-
-def test_valor_1995():
-    assert entero_a_romano(1995) == "MCMXCV"
+def test_descomposicion_336():
+    assert entero_a_romano(336) == ['0000', '300', '30', '6'] 
